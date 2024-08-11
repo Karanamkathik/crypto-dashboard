62678a5c32cbc78c7b65e4e515e0c7a9abf75a4f
Karanam karthik <142888732+Karanamkathik@users.noreply.github.com>
2024-08-11 21:15:28 +0530

    topButton

--- src/components/Common/BackToTop/index.js
+++ src/components/Common/BackToTop/index.js
@@ -10,7 +10,9 @@ let mybutton = document.getElementById("myBtn");
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+  if (document.body.scrollTop > 300 ||
+     document.documentElement.scrollTop > 300) 
+     {
     mybutton.style.display = "flex";
   } else {
     mybutton.style.display = "none";
@@ -18,12 +20,16 @@ function scrollFunction() {
 }
 
 
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
+
+  // document.body.scrollTop = 0;
+  // document.documentElement.scrollTop = 0;
+
   return (
-    <div className='back-to-top' id='myBtn' onClick={()=>topFunction()}>
+    <div className='back-to-top' id='myBtn' onClick={()=>
+      {document.body.scrollTop = 0;
+      document.documentElement.scrollTop = 0;
+    }}
+    >
         <ExpandLessRoundedIcon  style={{color :"var(--blue)"}}/>
     </div>
   )
