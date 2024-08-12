6e59da8e7e928462c8743345f16b94f0486e2098
Karanam karthik <142888732+Karanamkathik@users.noreply.github.com>
2024-08-12 20:44:10 +0530

    errorscorrected

--- src/components/Common/BackToTop/index.js
+++ src/components/Common/BackToTop/index.js
@@ -1,38 +1,75 @@
-import React from 'react'
+// import React from 'react'
+// import ExpandLessRoundedIcon from "@mui/icons-material/ExpandLessRounded";
+// import "./style.css";
+
+// function BackToTop() {
+    
+// let mybutton = document.getElementById("myBtn");
+
+
+// window.onscroll = function() {scrollFunction()};
+
+// function scrollFunction() {
+//   if (document.body.scrollTop > 300 ||
+//      document.documentElement.scrollTop > 300) 
+//      {
+//     mybutton.style.display = "flex";
+//   } else {
+//     mybutton.style.display = "none";
+//   }
+// }
+
+
+
+//   // document.body.scrollTop = 0;
+//   // document.documentElement.scrollTop = 0;
+
+//   return (
+//     <div className='back-to-top' id='myBtn' onClick={()=>
+//       {document.body.scrollTop = 0;
+//       document.documentElement.scrollTop = 0;
+//     }}
+//     >
+//         <ExpandLessRoundedIcon  style={{color :"var(--blue)"}}/>
+//     </div>
+//   )
+// }
+
+// export default BackToTop;
+
+import React, { useEffect } from 'react';
 import ExpandLessRoundedIcon from "@mui/icons-material/ExpandLessRounded";
 import "./style.css";
 
 function BackToTop() {
-    
-let mybutton = document.getElementById("myBtn");
+  
+  useEffect(() => {
+    const mybutton = document.getElementById("myBtn");
 
+    const scrollFunction = () => {
+      if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+        mybutton.style.display = "flex";
+      } else {
+        mybutton.style.display = "none";
+      }
+    };
 
-window.onscroll = function() {scrollFunction()};
+    window.onscroll = scrollFunction;
 
-function scrollFunction() {
-  if (document.body.scrollTop > 300 ||
-     document.documentElement.scrollTop > 300) 
-     {
-    mybutton.style.display = "flex";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-
-
-
-  // document.body.scrollTop = 0;
-  // document.documentElement.scrollTop = 0;
+    // Cleanup function to remove the event listener when the component is unmounted
+    return () => {
+      window.onscroll = null;
+    };
+  }, []);
 
   return (
-    <div className='back-to-top' id='myBtn' onClick={()=>
-      {document.body.scrollTop = 0;
+    <div className='back-to-top' id='myBtn' onClick={() => {
+      document.body.scrollTop = 0;
       document.documentElement.scrollTop = 0;
-    }}
-    >
-        <ExpandLessRoundedIcon  style={{color :"var(--blue)"}}/>
+    }}>
+      <ExpandLessRoundedIcon style={{color: "var(--blue)"}}/>
     </div>
-  )
+  );
 }
 
 export default BackToTop;
